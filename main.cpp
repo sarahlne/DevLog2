61e46fc34913a2f488568a1c8c3c41a3d925d878
Titouan <tpoquillon@gmail.com>
2019-05-23 10:31:54 +0200

    Bonjour monde
    Signed-off-by: Titouan <tpoquillon@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+
+#include <cstdlib>
+#include <ctime>
+
+
+
+int main(){
+srand(time(NULL));
+std::cout << "Bonjour monde" << std::endl;
+
+
+return 0;
+}
